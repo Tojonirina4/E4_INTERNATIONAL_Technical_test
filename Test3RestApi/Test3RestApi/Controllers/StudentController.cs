8fb9de33a7e9adce757030cf1a7797ba271c115e
ramaroson <tojonirina88@gmail.com>
2021-11-16 01:52:38 +0400

    Test3RestApi Completed

---
+++ Test3RestApi/Test3RestApi/Controllers/StudentController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Test3RestApi.Models;
+
+namespace Test3RestApi.Controllers
+{
+
+    [Route("api/students")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        string xmlFile = "../Test3RestApi/data/StudentList.xml";
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> GetAllStudents()
+        {
+            XDocument xdoc = XDocument.Load(xmlFile);
+            var students = xdoc.Root.Descendants("Student").Select(x => new Student
+            {
+                Id = x.Attribute("Id").Value,
+                Fullname = x.Element("Fullname").Value,
+                Nationality = x.Element("Nationality").Value,
+            }).ToList();
+            return Ok(students);
+        }
+
+        //POST api/students
+        [HttpPost]
+        public ActionResult<Student> AddStudent([FromBody] Student student)
+        {
+            student.Id = Guid.NewGuid().ToString("N");
+            XDocument xdoc = XDocument.Load(xmlFile);
+            var xelement = new XElement("Student", new XAttribute("Id", student.Id),
+                new XElement("Fullname", student.Fullname), new XElement("Nationality", student.Nationality));
+            xdoc.Root.Add(xelement);
+            xdoc.Save(xmlFile);
+            return Ok(student);
+        }
+
+        //DELETE api/students/4
+        [HttpDelete("{id}")]
+        public ActionResult RemoveStudent(string id)
+        {
+            var xdoc = XDocument.Load(xmlFile);
+            var selectedStudent = xdoc.Root.Descendants("Student").FirstOrDefault(x => x.Attribute("Id").Value == id);
+            if (selectedStudent != null)
+            {
+                selectedStudent.Remove();
+                xdoc.Save(xmlFile);
+            }                
+            return Ok("Record Deleted");
+        }
+    }
+}
---
+++ Test3RestApi/Test3RestApi/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Test3RestApi.Models
+{
+    public class Student
+    {       
+        public string Id { get; set; }
+        [Required]
+        public string Fullname { get; set; }
+        public string Nationality { get; set; }
+    }
+}
